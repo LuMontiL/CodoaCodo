548897aaa607f43eb6b69059bd2c9f295cf62bf1
LourdesMontiLeloutre <lumontileloutre@gmail.com>
2022-05-13 22:14:12 -0300

    html y css registro + prueba funcion flecha + ajustes responsive

--- js/codoacodo.js
+++ js/codoacodo.js
@@ -2,6 +2,7 @@
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
 //Ejecuto salida
 document.getElementById("boton").addEventListener("click", open_close_menu);
+//asi sera? buscar en google-
 
 
 // Declaro variables para menu desplegable
@@ -13,9 +14,24 @@ btn_open=0;
 //Variable para salir del menu desplegado
 var exit =document.getElementById ("boton");
 var exit=0;
+var exit1=document.body;
+var exit1=0;
 
+//tengo que hacer que se pueda sair del menu, haciendo click en el body.
 //Evento para mostrar y ocultar el menu. 
+
+
 function open_close_menu () {   
+        if(btn_open==0){
+            menu1.classList.toggle("menu_side_move");
+            btn_open=1;
+           } else if (exit==0 && exit1==0 && btn_open==1){
+            menu1.classList.toggle("menu_side_move");
+            btn_open=0;   
+           }
+    }  
+
+   /* var open_close_menu = () => {
         if(btn_open==0){
             menu1.classList.toggle("menu_side_move");
             btn_open=1;
@@ -23,4 +39,4 @@ function open_close_menu () {
             menu1.classList.toggle("menu_side_move");
             btn_open=0;   
            }
-    }  
+    }*/
